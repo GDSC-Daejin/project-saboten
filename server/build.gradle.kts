975679a66ee03d079d49a4c08f8d0501f3cafbc1
Harry Park <jungsanmango@gmail.com>
2022-02-02 00:09:36 +0900

    [Server] Swagger + UI Setup

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
     testImplementation(kotlin("test"))
 }
 
