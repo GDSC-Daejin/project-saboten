0abed0d21e647a4507251e7a6f6f1a8a8f7cb82a
Harry Park <jungsanmango@gmail.com>
2022-02-20 04:36:00 +0900

    [feature/remove-useless-codes]: UserController Api Response 테스트

--- server/src/main/java/backend/controller/UserController.java
+++ server/src/main/java/backend/controller/UserController.java
@@ -1,16 +1,18 @@
 package backend.controller;
 
+import common.model.ApiResponse;
 import common.model.User;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 class UserController {
-    @ApiOperation(value="유저정보조회 Api 테스트", notes = "임시 테스트용")
+    @ApiOperation(value = "유저정보조회 Api 테스트", notes = "임시 테스트용")
     @GetMapping("/api/users/{id}")
-    public ResponseEntity<User> getUser() {
-        return ResponseEntity.ok(new User(0L, "", ""));
+    public ResponseEntity<ApiResponse<User>> getUser(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(new ApiResponse(null, new User(id, "", ""), ""));
     }
 }
