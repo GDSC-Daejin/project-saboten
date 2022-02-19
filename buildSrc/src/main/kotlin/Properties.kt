5d6a7fb2373d8dc2240cfb5508fdc9be19c435bf
Harry Park <jungsanmango@gmail.com>
2022-02-20 01:31:15 +0900

    [feature/SBOT-42]: 새 GCP / Firebase 프로젝트 연결 테스트

--- buildSrc/src/main/kotlin/Properties.kt
+++ buildSrc/src/main/kotlin/Properties.kt
@@ -3,7 +3,7 @@ object Properties {
     const val androidMinSDK = 24
     const val androidTargetSDK = 31
     const val androidCompileSDK = 31
-    const val androidPackageName = "app.saboten.android"
+    const val androidPackageName = "app.saboten.androidApp"
 
     const val androidAppVersionName = "1.00.00"
     val androidAppVersionCode = androidAppVersionName.filter { it.isDigit() }.toInt()
