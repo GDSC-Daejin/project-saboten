fa49a810850ecf42ea0f1208bad732b4975c2526
Harry Park <jungsanmango@gmail.com>
2022-02-20 04:43:46 +0900

    [feature/remove-useless-codes]: 일부 수정

--- common/src/commonMain/kotlin/common/model/ApiResponse.kt
+++ common/src/commonMain/kotlin/common/model/ApiResponse.kt
@@ -3,7 +3,7 @@ package common.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-class ApiResponse<T>(
+data class ApiResponse<T>(
     val error : String?,
     val data : T,
     val message : String?
--- server/src/main/java/backend/controller/UserController.java
+++ server/src/main/java/backend/controller/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 class UserController {
     @ApiOperation(value = "유저정보조회 Api 테스트", notes = "임시 테스트용")
     @GetMapping("/api/users/{id}")
-    public ResponseEntity<ApiResponse<User>> getUser(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(new ApiResponse(null, new User(id, "", ""), ""));
+    public ApiResponse<User> getUser(@PathVariable("id") Long id) {
+        return new ApiResponse(null, new User(id, "", ""), "");
     }
 }
