66dfc18d8044697a151d95b9c464e1cabe8c9a20
Harry Park <jungsanmango@gmail.com>
2022-03-09 22:42:18 +0900

    [feature/fix-kotlin-js-api-unresolved-issue]: CreateRoute.kt 쪽 generic 이슈 수정

--- web/src/main/kotlin/routes/CreateRoute.kt
+++ web/src/main/kotlin/routes/CreateRoute.kt
@@ -1,16 +1,17 @@
 package routes
 
 import react.FC
+import react.Props
 import react.RBuilder
 import react.createElement
 import react.router.Route
 
-fun RBuilder.route(
+fun <P : Props> RBuilder.route(
     to: String,
-    component: FC<*>
+    component: FC<P>
 ) {
     Route {
         attrs.path = to
-        attrs.element = createElement { component() }
+        attrs.element = createElement(component)
     }
 }
