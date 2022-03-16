d745d9680320a77045f91580b616cec906af7fd8
shinequasar <shinequasar@gmail.com>
2022-03-17 00:53:18 +0900

    [feature/SBOT-51] PostImage Entity 복합키로 생성

---
+++ server/src/main/java/backend/model/PostImageEntity.java
@@ -0,0 +1,27 @@
+package backend.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity @IdClass(PostImagePK.class)
+@Table(name = "TB_PostImage")
+public class PostImageEntity {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="post_id", nullable = false)
+    public PostEntity post;
+
+    @Id
+    @Column(name = "post_image_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postImageId;
+
+
+    @Column(name="post_image", nullable = false)
+    private String postImage;
+}
---
+++ server/src/main/java/backend/model/PostImagePK.java
@@ -0,0 +1,32 @@
+package backend.model;
+
+import lombok.Builder;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class PostImagePK implements Serializable {
+
+    private Long postImageId;
+    private Long postId;
+
+    @Builder
+    public PostImagePK(Long postImageId, Long postId) {
+        this.postImageId = postImageId;
+        this.postId = postId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PostImagePK)) return false;
+        PostImagePK that = (PostImagePK) o;
+        return Objects.equals(postImageId, that.postImageId) && Objects.equals(postId, that.postId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postImageId, postId);
+    }
+}
