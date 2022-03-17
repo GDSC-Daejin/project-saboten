b73a52689daad9f9c97deb3d59c99b77b2d670cc
shinequasar <shinequasar@gmail.com>
2022-03-17 11:23:47 +0900

    [feature/SBOT-51] PostLike Entity 복합키로 생성

---
+++ server/src/main/java/backend/model/PostLikeEntity.java
@@ -0,0 +1,22 @@
+package backend.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity @IdClass(PostLikePK.class)
+@Table(name = "TB_PostLike")
+public class PostLikeEntity {
+    @Id
+    @ManyToOne
+    @JoinColumn(name="post_id", nullable = false)
+    public PostEntity post;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    public UserEntity user;
+}
---
+++ server/src/main/java/backend/model/PostLikePK.java
@@ -0,0 +1,30 @@
+package backend.model;
+
+import lombok.Builder;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PostLikePK implements Serializable {
+    private Long postId;
+    private Long userId;
+
+    @Builder
+    public PostLikePK(Long postId, Long userId) {
+        this.postId = postId;
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PostLikePK)) return false;
+        PostLikePK that = (PostLikePK) o;
+        return Objects.equals(postId, that.postId) && Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postId, userId);
+    }
+}
