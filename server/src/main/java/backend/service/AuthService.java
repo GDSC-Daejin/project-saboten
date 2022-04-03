83fa1e05afabf86cbde43bf458dfb32e004a2ee8
boo105 <boo10500@naver.com>
2022-04-03 18:58:41 +0900

    [feature/SBOT-87]: [feat] jwt 재발급 구현 및 json 관련 예외 처리 추가

--- server/src/main/java/backend/service/AuthService.java
+++ server/src/main/java/backend/service/AuthService.java
@@ -7,25 +7,23 @@ import backend.model.user.RefreshTokenEntity;
 import backend.model.user.UserEntity;
 import backend.repository.user.RefreshTokenRepository;
 import backend.repository.user.UserRepository;
+import common.message.BasicResponseMessage;
 import common.message.UserResponseMessage;
+import common.model.request.auth.TokenReissueRequest;
 import common.model.request.user.UserLoginTestRequest;
 import common.model.request.user.UserSignInRequest;
-import common.model.reseponse.ApiResponse;
 import common.model.reseponse.user.User;
 import common.model.reseponse.auth.JwtToken;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
@@ -41,6 +39,7 @@ public class AuthService {
         return userRepository.save(user).toDto();
     }
 
+    @Transactional
     public JwtToken login(UserLoginTestRequest userLoginTestRequest) {
         // 테스트용
         Long id = userLoginTestRequest.getId();
@@ -64,5 +63,43 @@ public class AuthService {
 
         // 5. 토큰 발급
         return jwtToken;
+    }
+
+    // TODO : 이미 로그인 상태면 exception 발생 해줘야함.
+    @Transactional
+    public JwtToken reissue(TokenReissueRequest tokenReissueRequest) {
+        // 1. Refresh Token 검증
+        if (!tokenProvider.validateToken(tokenReissueRequest.getRefreshToken())) {
+            throw new ApiException(BasicResponseMessage.INVALID_REFRESH_TOKEN);
+        }
+
+        // 2. Access Token 에서 Member ID 가져오기
+        Authentication authentication = tokenProvider.getAuthentication(tokenReissueRequest.getAccessToken());
+
+        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+        // TODO : 지금 UserFind 하는 것이 중복적으로 코드가 발생하는데 Controller Layer 에서 userService를 이용하여
+        // authService에 전달하는게 좋을듯?? (Entity가 mapsId 때문에 이렇긴한데....)
+        // 논의가 필요!!!!
+        String id = authentication.getName();
+        Optional<UserEntity> userEntity = userRepository.findById(Long.parseLong(id));
+        if(userEntity.isEmpty())
+            throw new ApiException(UserResponseMessage.USER_NOT_FOUND);
+
+        RefreshTokenEntity refreshToken = refreshTokenRepository.findByUser(userEntity.get());
+
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getRefreshToken().equals(tokenReissueRequest.getRefreshToken())) {
+            throw new ApiException(BasicResponseMessage.INVALID_REFRESH_TOKEN);
+        }
+
+        // 5. 새로운 토큰 생성
+        JwtToken newRefreshToken = tokenProvider.generateJwtToken(id, RoleType.USER);
+
+        // 6. 저장소 정보 업데이트
+        refreshToken.setRefreshToken(newRefreshToken.getRefreshToken());
+        refreshTokenRepository.save(refreshToken);
+
+        // 토큰 발급
+        return newRefreshToken;
     }
 }
