935adcb8bff464fb5e5192cc5b953f8bca1d43aa
Harry Park <jungsanmango@gmail.com>
2022-04-10 21:36:32 +0900

    [feature/SBOT-83]: PostDetailScreenViewModel.kt 에 Event 로직 추가

--- common-client/src/commonMain/kotlin/commonClient/domain/repository/PostRepository.kt
+++ common-client/src/commonMain/kotlin/commonClient/domain/repository/PostRepository.kt
@@ -1,4 +1,11 @@
 package commonClient.domain.repository
 
+import common.model.reseponse.post.Post
+import commonClient.data.LoadState
+import kotlinx.coroutines.flow.Flow
+
 interface PostRepository {
+
+    fun getPost(postId: Long): Flow<LoadState<Post>>
+
 }
--- common-client/src/commonMain/kotlin/commonClient/presentation/PostDetailScreenViewModel.kt
+++ common-client/src/commonMain/kotlin/commonClient/presentation/PostDetailScreenViewModel.kt
@@ -4,38 +4,51 @@ import common.model.reseponse.post.Post
 import commonClient.data.LoadState
 import commonClient.di.Inject
 import commonClient.di.Singleton
+import commonClient.domain.usecase.post.GetPostByIdUseCase
 import commonClient.presentation.PostDetailScreenViewModelDelegate.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.*
 
 
 interface PostDetailScreenViewModelDelegate : UnidirectionalViewModelDelegate<State, Effect, Event> {
 
     data class State(
-        val postState : LoadState<Post> = LoadState.loading()
+        val postState: LoadState<Post> = LoadState.loading()
     )
 
     sealed interface Effect
 
-    sealed interface Event
+    sealed interface Event {
+        class SetPost(val post: Post) : Event
+        class LoadPost(val postId: Long) : Event
+    }
 
 }
 
 @Singleton
 class PostDetailScreenViewModel @Inject constructor(
-
+    private val getPostByIdUseCase: GetPostByIdUseCase
 ) : PlatformViewModel(), PostDetailScreenViewModelDelegate {
 
     private val effectChannel = Channel<Effect>(Channel.UNLIMITED)
 
     override val effect: Flow<Effect> = effectChannel.receiveAsFlow()
 
+    private val postState = MutableStateFlow<LoadState<Post>>(LoadState.loading())
+
     override val state: StateFlow<State>
         get() = TODO("Not yet implemented")
 
     override fun event(e: Event) {
-        TODO("Not yet implemented")
+        when (e) {
+            is Event.LoadPost -> {
+                getPostByIdUseCase(e.postId)
+                    .onEach { postState.emit(it) }
+                    .launchIn(platformViewModelScope)
+            }
+            is Event.SetPost -> {
+                postState.tryEmit(LoadState.success(e.post))
+            }
+        }
     }
 }
