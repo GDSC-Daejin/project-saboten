293f193b85f23660aceb9aa3bb3047e2a40969bc
shinequasar <shinequasar@gmail.com>
2022-04-13 21:58:43 +0900

    [feature/SBOT-110] 내가 쓴 게시글 DTO

---
+++ common/src/commonMain/kotlin/common/model/reseponse/post/read/PostReadedResponse.kt
@@ -0,0 +1,17 @@
+package common.model.reseponse.post.read
+
+import common.model.reseponse.post.VoteResponse
+import common.model.reseponse.user.UserResponse
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+//내가 쓴 게시글 DTO
+@Serializable
+data class PostReadedResponse (
+    @SerialName("id") val id: Long,
+    @SerialName("text") val text: String,
+    @SerialName("author") val author: UserResponse,
+    @SerialName("votes") val votes: List<VoteResponse>,
+    @SerialName("created_at") val createdAt: String,
+    @SerialName("updated_at") val updatedAt: String?
+)
