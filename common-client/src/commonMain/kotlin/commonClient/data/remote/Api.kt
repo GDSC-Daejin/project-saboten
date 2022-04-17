9b7fd651214475732744dbf27441f1c46eb3c255
Harry Park <jungsanmango@gmail.com>
2022-04-17 17:34:53 +0900

    [feature/client-token-enhancement]: Token, Api Response 쪽 로직 개선

--- common-client/src/commonMain/kotlin/commonClient/data/remote/Api.kt
+++ common-client/src/commonMain/kotlin/commonClient/data/remote/Api.kt
@@ -19,76 +19,38 @@ suspend inline fun <reified T> Api.responseGet(
     suffixUrl: Any = "/",
     block: HttpRequestBuilder.() -> Unit = {}
 ): ApiResponse<T> {
-    return runCatching {
-        httpClient.get {
-            url { encodedPath = "$prefixUrl$suffixUrl" }
-            block()
-        }
-    }.fold({
-        it.body()
-    }, {
-        when (it) {
-            is ClientRequestException -> it.response.body()
-            is ServerResponseException -> it.response.body()
-            else -> throw it
-        }
-    })
+    return httpClient.get {
+        url { encodedPath = "$prefixUrl$suffixUrl" }
+        block()
+    }.body()
 }
 
 suspend inline fun <reified T> Api.responsePost(
     suffixUrl: Any = "/",
     block: HttpRequestBuilder.() -> Unit = {}
 ): ApiResponse<T> {
-    return runCatching {
-        httpClient.post {
-            url { encodedPath = "$prefixUrl$suffixUrl" }
-            block()
-        }
-    }.fold({
-        it.body()
-    }, {
-        when (it) {
-            is ClientRequestException -> it.response.body()
-            is ServerResponseException -> it.response.body()
-            else -> throw it
-        }
-    })
+    return httpClient.post {
+        url { encodedPath = "$prefixUrl$suffixUrl" }
+        block()
+    }.body()
 }
 
 suspend inline fun <reified T> Api.responsePatch(
     suffixUrl: Any = "/",
     block: HttpRequestBuilder.() -> Unit = {}
 ): ApiResponse<T> {
-    return runCatching {
-        httpClient.patch {
-            url { encodedPath = "$prefixUrl$suffixUrl" }
-            block()
-        }
-    }.fold({
-        it.body()
-    }, {
-        when (it) {
-            is ClientRequestException -> it.response.body()
-            is ServerResponseException -> it.response.body()
-            else -> throw it
-        }
-    })
+    return httpClient.patch {
+        url { encodedPath = "$prefixUrl$suffixUrl" }
+        block()
+    }.body()
 }
 
 suspend inline fun <reified T> Api.responseDelete(
     suffixUrl: Any = "/",
     block: HttpRequestBuilder.() -> Unit = {}
 ): ApiResponse<T> {
-    return runCatching {
-        httpClient.delete {
-            url { encodedPath = "$prefixUrl$suffixUrl" }
-            block()
-        }
-    }.fold({ it.body() }, {
-        when (it) {
-            is ClientRequestException -> it.response.body()
-            is ServerResponseException -> it.response.body()
-            else -> throw it
-        }
-    })
+    return httpClient.delete {
+        url { encodedPath = "$prefixUrl$suffixUrl" }
+        block()
+    }.body()
 }
