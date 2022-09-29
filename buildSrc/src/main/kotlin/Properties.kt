98c192c67b0de4f792d0f31b64ee1c52bc37e961
Harry Park <jungsanmango@gmail.com>
2022-09-29 21:49:42 +0900

    [feature/koin-refactoring]: Version Update

--- buildSrc/src/main/kotlin/Properties.kt
+++ buildSrc/src/main/kotlin/Properties.kt
@@ -1,8 +1,8 @@
 object Properties {
 
     const val androidMinSDK = 24
-    const val androidTargetSDK = 32
-    const val androidCompileSDK = 32
+    const val androidTargetSDK = 33
+    const val androidCompileSDK = 33
     const val androidPackageName = "app.saboten.androidApp"
 
     const val androidAppVersionName = "1.00.00"
