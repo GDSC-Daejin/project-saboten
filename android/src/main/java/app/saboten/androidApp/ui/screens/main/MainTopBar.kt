da0c265a281311832323eee324b96f93a95c87c9
soopeach <hsjeon584@gmail.com>
2022-11-25 13:03:01 +0900

    [feature/SBOT-195]: [feature/SBOT-195] MainTopBar 구현
    BasicTopBar를 이용

---
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/MainTopBar.kt
@@ -0,0 +1,47 @@
+package app.saboten.androidApp.ui.screens.main
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.saboten.androidUi.image.sabotenlogo.SabotenLogo
+import app.saboten.androidUi.bars.BasicTopBar
+import app.saboten.androidUi.styles.SabotenColors
+
+@Composable
+fun MainTopBar() {
+
+    BasicTopBar(
+        title = {
+            Icon(
+                imageVector = SabotenLogo.SabotenLogo,
+                contentDescription = "선인장 로그",
+                tint = SabotenColors.green500
+            )
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Notifications,
+                    contentDescription = "알림",
+                    modifier = Modifier.size(26.dp),
+                    tint = SabotenColors.green500
+                )
+
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "검색",
+                    modifier = Modifier.size(26.dp),
+                    tint = SabotenColors.green500
+                )
+            }
+        }
+    )
+}
