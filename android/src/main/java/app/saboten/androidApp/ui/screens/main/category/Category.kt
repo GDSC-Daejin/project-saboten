6dfbf3a453dbdf8c37709dd75209550e3ea4cfc9
soopeach <hsjeon584@gmail.com>
2023-01-19 16:40:40 +0900

    [feature/SBOT-215]: Category.kt 패키지 변경

--- android/src/main/java/app/saboten/androidApp/ui/screens/main/category/Category.kt
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/category/Category.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import app.saboten.androidUi.image.NetworkImage
 import app.saboten.androidUi.image.SabotenIconPack
