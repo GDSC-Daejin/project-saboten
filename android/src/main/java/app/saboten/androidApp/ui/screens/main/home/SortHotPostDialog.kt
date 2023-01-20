5d1df57ce64da476375b60fdf049ab03866ff71f
soopeach <hsjeon584@gmail.com>
2023-01-20 14:57:01 +0900

    [feature/SBOT-215]: LazyRow -> LazyHorizontalGrid로 변경

--- android/src/main/java/app/saboten/androidApp/ui/screens/main/home/SortHotPostDialog.kt
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/home/SortHotPostDialog.kt
@@ -12,7 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -76,10 +77,11 @@ fun SortHotPostDialog(
 
         DragBar()
 
-        LazyRow(
+        LazyHorizontalGrid(
             modifier = Modifier
                 .height(267.dp),
-            horizontalArrangement = Arrangement.spacedBy(36.dp)
+            horizontalArrangement = Arrangement.spacedBy(36.dp),
+            rows = GridCells.Fixed(1)
         ) {
 
             item {
