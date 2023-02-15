ab23e4c5186a89571a7f09c40cbeb4c93e858cfe
soopeach <hsjeon584@gmail.com>
2023-02-15 23:34:18 +0900

    [feature/profileScreen]: 비로그인 시 로그인 안내 화면 변경

--- android/src/main/java/app/saboten/androidApp/ui/screens/main/profile/ProfileScreen.kt
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/profile/ProfileScreen.kt
@@ -8,40 +8,33 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForwardIos
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import app.saboten.androidApp.ui.destinations.DetailPostScreenDestination
 import app.saboten.androidApp.ui.destinations.SettingsScreenDestination
 import app.saboten.androidApp.ui.providers.LocalMeInfo
-import app.saboten.androidApp.ui.providers.MeInfo
 import app.saboten.androidApp.ui.screens.main.post.LargePostCard
 import app.saboten.androidUi.bars.BasicTopBar
-import app.saboten.androidUi.buttons.SmallOutlinedButton
 import app.saboten.androidUi.image.NetworkImage
-import app.saboten.androidUi.lists.BasicListItem
+import app.saboten.androidUi.image.sabotenlogo.Google
+import app.saboten.androidUi.image.sabotenlogo.SabotenIcons
 import app.saboten.androidUi.scaffolds.BasicScaffold
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.pagerTabIndicatorOffset
-import com.google.accompanist.pager.rememberPagerState
+import app.saboten.androidUi.styles.SabotenColors
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import commonClient.data.LoadState
 import commonClient.domain.entity.post.Post
-import commonClient.domain.entity.user.UserInfo
 import commonClient.presentation.main.ProfileScreenViewModel
-import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import org.orbitmvi.orbit.compose.collectAsState
 
@@ -186,18 +179,36 @@ private fun ProfileBannerUi() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(20.dp),
+                    imageVector = SabotenIcons.Google,
+                    contentDescription = "구글 로고"
+                )
+                Spacer(modifier = Modifier.padding(start = 4.dp))
+                Text(
+                    text = "로그인",
+                    style = MaterialTheme.typography.h6,
+                    fontWeight = FontWeight.Bold
+                )
+                Icon(
+                    modifier = Modifier.size(14.dp),
+                    imageVector = Icons.Rounded.ArrowForwardIos,
+                    contentDescription = "로그인",
+                )
+            }
+            
+            Spacer(modifier = Modifier.padding(top = 4.dp))
+            
             Text(
-                text = "로그인이 필요합니다.",
-                style = MaterialTheme.typography.h6,
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-            SmallOutlinedButton(
-                text = "로그인",
-                onClick = {
-
-                }
+                modifier = Modifier.fillMaxWidth(),
+                text = "로그인 후 더 많은 서비스를 이용해보세요",
+                fontSize = 12.sp,
+                color = SabotenColors.grey500
             )
 
         }
