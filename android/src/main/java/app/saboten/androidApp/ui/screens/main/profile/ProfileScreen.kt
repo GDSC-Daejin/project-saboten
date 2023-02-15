139c2d4d56833bd7e549280c2db0a7046e685d78
soopeach <hsjeon584@gmail.com>
2023-02-16 00:51:30 +0900

    [feature/profileScreen]: 게시글 정보가 담겨있는 Box Item 추가

--- android/src/main/java/app/saboten/androidApp/ui/screens/main/profile/ProfileScreen.kt
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/profile/ProfileScreen.kt
@@ -1,13 +1,16 @@
 package app.saboten.androidApp.ui.screens.main.profile
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.rounded.ArrowForwardIos
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
@@ -37,6 +40,12 @@ import commonClient.domain.entity.post.Post
 import commonClient.presentation.main.ProfileScreenViewModel
 import org.koin.androidx.compose.koinViewModel
 import org.orbitmvi.orbit.compose.collectAsState
+
+data class PostInfoState(
+    val postCount: Int,
+    val voteCount: Int,
+    val scrapCount: Int,
+)
 
 @Composable
 @Destination
@@ -93,6 +102,10 @@ private fun ProfileScreenContent(
 
                         ProfileBannerUi()
 
+                        // TODO: 로그인 여부에 따라 상태를 인자로 넘겨줄 지 결정해야함. -> 상태를 넘겨주지 않으면 비로그인 상태
+                        // PostInfoBox는 내가 쓴 게시글, 투표한 게시글, 스크랩한 게시글을 보여주는 화면
+                        PostInfoBox()
+
                         LazyRow(
                             contentPadding = PaddingValues(20.dp),
                         ) {
@@ -108,7 +121,10 @@ private fun ProfileScreenContent(
                                     shape = CircleShape,
                                     border = if (state.selectedCategoryId == itemId) null
                                     else {
-                                        BorderStroke(1.dp, MaterialTheme.colors.onBackground.copy(0.3f))
+                                        BorderStroke(
+                                            1.dp,
+                                            MaterialTheme.colors.onBackground.copy(0.3f)
+                                        )
                                     },
                                 ) {
 
@@ -201,9 +217,9 @@ private fun ProfileBannerUi() {
                     contentDescription = "로그인",
                 )
             }
-            
+
             Spacer(modifier = Modifier.padding(top = 4.dp))
-            
+
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 text = "로그인 후 더 많은 서비스를 이용해보세요",
@@ -253,4 +269,64 @@ private fun ProfileBannerUi() {
 
     }
 
+}
+
+@Composable
+fun PostInfoBox(state: PostInfoState? = null) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+            .height(90.dp)
+            .border(
+                width = 1.dp,
+                color = SabotenColors.grey200,
+                shape = RoundedCornerShape(10.dp)
+            ),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        if (state != null) {
+            TextWithCount("게시글", state.postCount)
+            TextWithCount("투표", state.voteCount)
+            TextWithCount("스크랩", state.scrapCount)
+        } else {
+            TextWithCount("게시글")
+            TextWithCount("투표")
+            TextWithCount("스크랩")
+        }
+
+
+    }
+}
+
+@Composable
+private fun TextWithCount(text: String, count: Int? = null) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        if (count != null) {
+            Text(
+                text = "$count",
+                fontSize = 20.sp,
+                color = SabotenColors.grey900
+            )
+        } else {
+            Icon(
+                imageVector = Icons.Outlined.Lock, contentDescription = "자물쇠",
+                tint = SabotenColors.grey600
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+        Text(
+            text = text,
+            fontSize = 10.sp,
+            color = SabotenColors.grey600
+        )
+    }
 }
