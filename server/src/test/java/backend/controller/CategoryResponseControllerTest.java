d20ecdbebaa73e49ac799a89883c0508b56d1115
boo105 <boo10500@naver.com>
2023-02-22 22:47:35 +0900

    [release-server/ec2]: backend prod 설정 배포

--- server/src/test/java/backend/controller/CategoryResponseControllerTest.java
+++ server/src/test/java/backend/controller/CategoryResponseControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @UTF8Config
-@ActiveProfiles("dev")
+@ActiveProfiles("prod")
 class CategoryResponseControllerTest {
     @Autowired
     private MockMvc mockMvc;
