20cbaa9184f8c83b3e47d5a5ae26e12a9b906dbf
soopeach <hsjeon584@gmail.com>
2023-04-15 22:44:22 +0900

    [feat/navigate_detail_post]: 댓글 창에 네비게이션바 패딩 적용

--- android/src/main/java/app/saboten/androidApp/ui/screens/main/post/detail/DetailPostPage.kt
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/post/detail/DetailPostPage.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -120,7 +120,8 @@ fun DetailPostPageContent(
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(20.dp),
+                        .padding(20.dp)
+                        .navigationBarsPadding(),
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     NetworkImage(
