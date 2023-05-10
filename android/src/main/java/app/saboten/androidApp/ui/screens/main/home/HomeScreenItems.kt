1b7618a84462985f0cffcc9efd9abf0c45574223
soopeach <hsjeon584@gmail.com>
2023-05-10 15:48:42 +0900

    [feature/banner]: 배너 ui 구현

--- android/src/main/java/app/saboten/androidApp/ui/screens/main/home/HomeScreenItems.kt
+++ android/src/main/java/app/saboten/androidApp/ui/screens/main/home/HomeScreenItems.kt
@@ -1,19 +1,15 @@
 package app.saboten.androidApp.ui.screens.main.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -25,12 +21,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import app.saboten.androidUi.bars.HeaderBar
 import app.saboten.androidUi.styles.SabotenColors
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import app.saboten.androidUi.image.NetworkImage
 import commonClient.data.LoadState
-import commonClient.domain.entity.post.Post
 import commonClient.presentation.main.HomeScreenState
 
 @Composable
@@ -57,54 +52,71 @@ fun HomeScreenTrendingItems(
                 modifier = Modifier
                     .fillMaxWidth()
                     .aspectRatio(0.96f)
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                SabotenColors.green500,
-                                SabotenColors.green800
-                            )
-                        )
-                    )
             ) {
 
                 HorizontalPager(
                     state = pagerState,
-                    count = banner.data.size,
+                    pageCount = banner.data.size,
                     modifier = Modifier.fillMaxSize()
-                ) {
-                    val item = banner.data[it]
+                ){ page ->
+
+                    val item = banner.data[page]
+
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    colors = listOf(
+                                        getColor(banner.data[page].startColor),
+                                        getColor(banner.data[page].endColor)
+                                    )
+                                )
+                            )
                     ) {
                         Box(
                             modifier = Modifier
                                 .fillMaxSize()
                         ) {
+                            NetworkImage(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                url = item.iconUrl,
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .align(Alignment.BottomStart)
+                                    .padding(20.dp)
+                            ) {
+                                Text(
+                                    text = item.subtitle,
+                                    style = MaterialTheme.typography.body1,
+                                    color = Color.White,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+
                             Column(
                                 modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(
-                                        start = 16.dp,
-                                        end = 16.dp,
-                                        bottom = 16.dp
-                                    ),
-                                verticalArrangement = Arrangement.Bottom
+                                    .fillMaxHeight(0.75f)
+                                    .align(Alignment.BottomStart)
+                                    .padding(20.dp),
                             ) {
+                                Text(
+                                    text = "오늘의 ${item.category} 고민거리",
+                                    style = MaterialTheme.typography.body1,
+                                    color = Color.White,
+                                    textAlign = TextAlign.Center
+                                )
+                                Spacer(modifier = Modifier.height(1.dp))
                                 Text(
                                     text = item.title,
                                     style = MaterialTheme.typography.h4,
                                     color = Color.White,
                                     textAlign = TextAlign.Center
                                 )
-                                Spacer(modifier = Modifier.height(8.dp))
-                                Text(
-                                    text = item.text,
-                                    style = MaterialTheme.typography.body1,
-                                    color = Color.White,
-                                    textAlign = TextAlign.Center
-                                )
                             }
+
                         }
                     }
                 }
@@ -117,10 +129,13 @@ fun HomeScreenTrendingItems(
 
                     Text(
                         modifier = Modifier
-                            .background(SabotenColors.grey1000.copy(0.3f), RoundedCornerShape(100.dp))
+                            .background(
+                                SabotenColors.grey1000.copy(0.3f),
+                                RoundedCornerShape(100.dp)
+                            )
                             .padding(horizontal = 12.dp, vertical = 4.dp),
                         textAlign = TextAlign.Center,
-                        text = "${pagerState.currentPage + 1} / ${pagerState.pageCount}",
+                        text = "${pagerState.currentPage + 1} / ${banner.data.size}",
                         style = MaterialTheme.typography.body2,
                         color = SabotenColors.grey100
                     )
@@ -139,4 +154,8 @@ fun HomeScreenTrendingItems(
         }
     }
 
+}
+
+fun getColor(colorString: String): Color {
+    return Color(android.graphics.Color.parseColor("#$colorString"))
 }
